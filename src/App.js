76ab63a9a2fbdd641830a7e3665eee3570e4ba30
sodiadrhain <adesojiawobajo@gmail.com>
2020-04-12 18:55:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import "./App.css";
+
+class App extends Component {
+
+render(){
+
+  return(
+    <div className="main">
+      <h1>Shell</h1>
+      <p>We are launching soon...</p>
+    </div>
+  );
+}
+
+}
+
+export default App;
