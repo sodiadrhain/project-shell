ee633c1c690727b5f049f77e01f945f827d5781b
Adebisi Ayomide <usiwoma.oghenovo@rosabon-finance.net>
2020-04-12 21:28:39 +0100

    ayomide-first-commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, {Component} from "react";
 import "./App.css";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./pages/Home/Home";
 
 class App extends Component {
-
 render(){
-
   return(
-    <div className="main">
-      <h1>Shell</h1>
-      <p>We are launching soon...</p>
-    </div>
+    <Router>
+    <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+    </Switch>
+  </Router>
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row home-coming align-items-center">
+        <div className="col text-center">
+          <div className="content">
+            <h1>Something new coming soon (The-Shell).</h1>
+            <a
+              className="btn btn-outline-primary mt-4"
+              href="http://www.bit.ly"
+            >
+              Visit www.unyk.tv
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
